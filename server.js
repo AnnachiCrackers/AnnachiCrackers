ae42c1bccd22e9effd6394451e7b1ad5464ed0d8
AmalJoeSevin <amaljoesevin@gmail.com>
2025-07-25 16:33:00 +0530

    Final Testing pdf vew

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.jpg': 'image/jpeg',
+        '.png': 'image/png',
+        '.pdf': 'application/pdf'
+    };
+
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if(error.code === 'ENOENT') {
+                res.writeHead(404);
+                res.end('File not found');
+            } else {
+                res.writeHead(500);
+                res.end('Server error: ' + error.code);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+    console.log('To access from other devices on your network, use your computer\'s IP address:');
+    require('dns').lookup(require('os').hostname(), (err, add) => {
+        if (!err) console.log(`http://${add}:${PORT}/`);
+    });
+});
